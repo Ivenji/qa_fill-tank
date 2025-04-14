1e27bae3c1c08f1c72322ee675633d67683baf88
Ivenji <savgenkoe@gmail.com>
2025-04-13 21:11:05 -0400

    Solution

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,11 +1,105 @@
 'use strict';
 
 describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+  const { fillTank } = require('./fillTank');
 
-  it('should ', () => {
+  it('Якщо amount не передано, значить замовлено повний бак ', () => {
+    const customer = {
+      money: 100,
+      vehicle: {
+        maxTankCapacity: 50,
+        fuelRemains: 10,
+      },
+    };
+    const fuelPrice = 2;
 
+    fillTank(customer, fuelPrice);
+
+    expect(customer.vehicle.fuelRemains).toBe(50);
+    expect(customer.money).toBe(20);
   });
 
-  // write tests here
+  it('Якщо amount більше ніж вміщає бак, залий тільки те, що поміститься ',
+    () => {
+      const customer = {
+        money: 100,
+        vehicle: {
+          maxTankCapacity: 50,
+          fuelRemains: 10,
+        },
+      };
+      const fuelPrice = 2;
+
+      fillTank(customer, fuelPrice, 100);
+
+      expect(customer.vehicle.fuelRemains).toBe(50);
+      expect(customer.money).toBe(20);
+    });
+
+  it('Завжди заливаємо тільки те, за що клієнт може заплатити', () => {
+    const customer = {
+      money: 4,
+      vehicle: {
+        maxTankCapacity: 50,
+        fuelRemains: 10,
+      },
+    };
+    const fuelPrice = 2;
+
+    fillTank(customer, fuelPrice, 100);
+
+    expect(customer.vehicle.fuelRemains).toBe(12);
+    expect(customer.money).toBe(0);
+  });
+
+  it('Округлюй обсяг залитого палива вниз до десятих ', () => {
+    const customer = {
+      money: 6,
+      vehicle: {
+        maxTankCapacity: 50,
+        fuelRemains: 15.5,
+      },
+    };
+    const fuelPrice = 2;
+
+    fillTank(customer, fuelPrice, 3.14159);
+
+    expect(customer.vehicle.fuelRemains).toBe(18.5);
+    expect(customer.money).toBe(0);
+  });
+
+  it('Якщо вийшло < 2 літрів, взагалі не заправляй клієнта ', () => {
+    const customer = {
+      money: 100,
+      vehicle: {
+        maxTankCapacity: 50,
+        fuelRemains: 49,
+      },
+    };
+    const fuelPrice = 2;
+
+    fillTank(customer, fuelPrice, 1);
+
+    expect(customer.vehicle.fuelRemains).toBe(49);
+    expect(customer.money).toBe(100);
+  });
+
+  it(
+    `Вартість заправленого пального округли до сотих
+  (до найближчого значення)`,
+    () => {
+      const customer = {
+        money: 100,
+        vehicle: {
+          maxTankCapacity: 50,
+          fuelRemains: 40,
+        },
+      };
+      const fuelPrice = 2.555;
+
+      fillTank(customer, fuelPrice, 10);
+
+      expect(customer.vehicle.fuelRemains).toBe(50);
+      expect(customer.money).toBe(74.45);
+    });
 });
